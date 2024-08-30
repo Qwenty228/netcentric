d6939668b8a20dcc46f3270332de51b24f94a621
talent <probablyjustwind@gmail.com>
2024-08-30 20:18:10 +0700

    Instantiate small boats at mouse click

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -1,7 +1,26 @@
 extends Camera3D
+
+@export var grid_map: GridMap
+@export var boat_manager: Node3D
+
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
-	ray_cast_3d.target_position = project_local_ray_normal(mouse_position)
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 500
+	#ray_cast_3d.force_raycast_update()
+	if ray_cast_3d.is_colliding():
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			if Input.is_action_just_pressed("click"):
+				var collision_point = ray_cast_3d.get_collision_point()
+				var cell = grid_map.local_to_map(collision_point) 
+				if grid_map.get_cell_item(cell) == 1:
+					grid_map.set_cell_item(cell, 2)
+					var tile_position = grid_map.map_to_local(cell)
+					boat_manager.build_boat(tile_position)
+					
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
--- boat_manager.gd
+++ boat_manager.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @export var small_boat: PackedScene 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func build_boat(boat_position: Vector3) -> void:
 	var new_small_boat = small_boat.instantiate()
 	add_child(new_small_boat)
+	new_small_boat.global_position = boat_position
