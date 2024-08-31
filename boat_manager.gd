0b8fd5d18d919c74c15ab1f76c156fe5b112a263
talent <probablyjustwind@gmail.com>
2024-08-31 10:28:40 +0700

    Rotate boats

--- boat_manager.gd
+++ boat_manager.gd
@@ -7,21 +7,27 @@ extends Node3D
 	set(boat_type):
 		print("set boat type to ", boat_type)
 		current_boat_type = boat_type
+
+var rotation_angle:float = 0.0
 	
 func _process(delta:float) -> void:
 	if Input.is_action_just_pressed("boat1"):
 		current_boat_type = boat_1
-		print(current_boat_type)
 		
 	if Input.is_action_just_pressed("boat4"):
 		current_boat_type = boat_4
-		print(current_boat_type)
+		
+	if Input.is_action_just_pressed("rotate"):
+		rotation_angle -= PI/2
 
 func build_boat(boat_position: Vector3) -> Node3D:
 	if current_boat_type != null:
 		var new_boat = current_boat_type.instantiate()
 		add_child(new_boat)
 		new_boat.global_position = boat_position
+		
+		new_boat.global_rotation.y = rotation_angle
+		print(new_boat.global_rotation)
 		return new_boat
 	return null
 
