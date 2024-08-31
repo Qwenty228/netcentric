ee60f0ba51f1fddecac20bc81a438af550d63996
talent <probablyjustwind@gmail.com>
2024-08-31 11:51:48 +0700

    Clear row of cells

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -26,9 +26,20 @@ func _process(delta: float) -> void:
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 500
 	ray_cast_3d.force_raycast_update()
 	if ray_cast_3d.is_colliding():
+		
 		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 		var collider = ray_cast_3d.get_collider()
-		if collider is GridMap:
+		print(collider)
+		if collider.is_in_group("boat"):
+			if Input.is_action_just_pressed("click"):
+				var temp_cell = grid_map.local_to_map(collider.get_child(0).global_position) 
+				for i in range(collider.get_parent().units):
+					grid_map.set_cell_item(temp_cell, 0)
+					temp_cell.z += 1
+				previous_boat = null
+				collider.queue_free()
+				
+		elif collider is GridMap:
 			if build_mode:
 				var collision_point = ray_cast_3d.get_collision_point()
 				var cell = grid_map.local_to_map(collision_point) 
@@ -38,6 +49,11 @@ func _process(delta: float) -> void:
 				if Input.is_action_just_pressed("click"):
 					if grid_map.get_cell_item(cell) != 2:
 						grid_map.set_cell_item(cell, 2)
+						var temp_cell = cell
+						previous_boat.get_child(0).use_collision = true
+						for i in range(previous_boat.units-1):
+							temp_cell.z += 1
+							grid_map.set_cell_item(temp_cell, 2)
 						previous_boat = null
 					elif grid_map.get_cell_item(cell) == 2:
 						grid_map.set_cell_item(cell, 0)
