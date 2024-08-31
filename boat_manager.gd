feb6c2c2b03a098a40891bf07480ba285c7cd386
talent <probablyjustwind@gmail.com>
2024-08-31 08:22:50 +0700

    Place and delete boats

--- boat_manager.gd
+++ boat_manager.gd
@@ -2,7 +2,13 @@ extends Node3D
 
 @export var small_boat: PackedScene 
 
-func build_boat(boat_position: Vector3) -> void:
+func build_boat(boat_position: Vector3) -> Node3D:
 	var new_small_boat = small_boat.instantiate()
 	add_child(new_small_boat)
 	new_small_boat.global_position = boat_position
+	return new_small_boat
+
+func delete_boat(boat_position: Vector3):
+	for child in get_children():
+		if child.global_position == boat_position:
+			child.queue_free()
